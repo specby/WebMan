155f7bf49f4cf142e1c1fea4a4c28b121c276836
Jiwon Jeong <own0206@gmail.com>
2022-11-25 20:20:17 +0900

    second commit

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "0d3f4cc99671e9a8d4937ca7a7c40c75";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in!", lat, lng );
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data)  =>{
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        const name = data.name;
+        city.innerText = data.name; 
+        weather.innerText = data.weather[0].main;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you");
